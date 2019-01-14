440f242d9ec12e9ffab8be600d721fdb8f5364be
Shelly Branch <sbranch@homeaway.com>
2019-01-14 22:32:32 +0000

    getRQ4.js has working code from step 4.

--- getRQ4.js
+++ getRQ4.js
@@ -3,42 +3,46 @@
  
 
 
-function getAndPrintHTML (options) {
+function getHTML (options, callback) { // 2. options receives the requestOptions 
 
-
-
- 
 
   var body = "";
 
   https.get(options, function (response) {
 
-  // set encoding of received data to UTF-8
+  
   response.setEncoding('utf8');
 
-  // the callback is invoked when a `data` chunk is received
+  
   response.on('data', function (data) {
 
     body = data.concat('\n');
 
-    console.log(body);
+
+
+    //console.log(body);
   	//console.log(data.concat('\n'));
     //console.log('Chunk Received. Length:', data.length);
   });
 
   // the callback is invoked when all of the data has been received
   // (the `end` of the stream)
-  response.on('end', function() {
-    console.log('Response stream complete.');
+  response.on('end', function() { // 3. when all of the data is received, the callback is invoked, printHTML. 
+    callback(body);
   });
 
 
 });
 }
+
+function printHTML (html) { //4. this function takes the data from the body and prints it.
+  console.log(html);
+}
+
  var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step2.html'
   };
 
 
-getAndPrintHTML(requestOptions)
+getHTML(requestOptions, printHTML) //  1.requestOptions is passeed to the options paremeter to trigger getHTML
