750253b893eff479d31d5f85c7da3dd09b9b730f
Shelly Branch <sbranch@homeaway.com>
2019-01-14 20:43:04 +0000

    this script uses the requirements for step3. A success message was generated.

---
+++ getRQ3.js
@@ -0,0 +1,45 @@
+ var https = require('https');   
+
+ 
+
+
+function getAndPrintHTML (options) {
+
+
+
+ 
+
+  var body = "";
+
+  https.get(options, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+
+    body = data.concat('\n');
+
+    console.log(body);
+  	//console.log(data.concat('\n'));
+    //console.log('Chunk Received. Length:', data.length);
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+
+});
+}
+ var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+
+getAndPrintHTML(requestOptions)
+  // };)
