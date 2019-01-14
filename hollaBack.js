5354afc0a16a57350f5ecbd2874c722e3733c4ce
Shelly Branch <sbranch@homeaway.com>
2019-01-14 23:23:05 +0000

    detailed notes have been added to the file

--- hollaBack.js
+++ hollaBack.js
@@ -1,10 +1,10 @@
-var getHTML = require('./http-functions');
+var getHTML = require('./http-functions'); // links to the http-functions.js file
 
 var options  = {
   host: 'sytantris.github.io',
   path: '/http-examples/step5.html'
 };
-function printHTML (html) { 
+function printHTML (html) { // this is the callback that is then activated to log a print out of the data.
   console.log(html);
 }
-getHTML(options, printHTML);
+getHTML(options, printHTML); // 1 . The options object with the host and path is passed in to getHTML to trigger getHTML
